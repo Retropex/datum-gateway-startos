b0d0396f0eab17c40f6d1608d14ac5a3e34bca39
Dominion5254 <musashidisciple@proton.me>
2024-11-26 09:17:39 -0700

    Fix/migration (#6)
    * fix migration
    
    * set configured: false

--- scripts/procedures/migrations.ts
+++ scripts/procedures/migrations.ts
@@ -1,4 +1,24 @@
-import { compat, types as T } from "../deps.ts";
+import { compat, matches, types as T } from "../deps.ts";
 
-export const migration: T.ExpectedExports.migration = compat.migrations
-    .fromMapping({}, "0.3.5" );
+export const migration: T.ExpectedExports.migration =
+  compat.migrations.fromMapping(
+    {
+      "0.2.1": {
+        up: compat.migrations.updateConfig(
+          (config) => {
+            return config;
+          },
+          false,
+          { version: "0.2.1", type: "up" }
+        ),
+        down: compat.migrations.updateConfig(
+          (config) => {
+            return config;
+          },
+          false,
+          { version: "0.2.1", type: "down" }
+        ),
+      },
+    },
+    "0.2.1"
+  );
