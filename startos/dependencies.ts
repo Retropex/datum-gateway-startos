3ea2343ad81234f75c5f30742b2048fbafcad530
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2025-03-03 13:37:16 -0500

    add rpc for bitcoin dep health check instead of separate health check

--- startos/dependencies.ts
+++ startos/dependencies.ts
@@ -4,6 +4,6 @@ export const setDependencies = sdk.setupDependencies(async ({ effects }) => ({
   bitcoind: {
     kind: 'running',
     versionRange: '>=28.0.0:1-knots',
-    healthChecks: [],
+    healthChecks: ['primary'],
   },
 }))
