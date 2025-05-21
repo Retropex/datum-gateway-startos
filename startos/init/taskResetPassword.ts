3405f8d4cb56831ca12ed0d5477772924feb5c96
Matt Hill <mattnine@protonmail.com>
2025-05-20 22:11:31 -0600

    beta 23 and always require password

--- startos/init/taskResetPassword.ts
+++ startos/init/taskResetPassword.ts
@@ -2,9 +2,9 @@ import { resetPassword } from '../actions/resetPassword'
 import { configJson } from '../fileModels/config.json'
 import { sdk } from '../sdk'
 
-export const taskResetPassword = sdk.setupOnInstall(async (effects) => {
+export const taskResetPassword = sdk.setupOnInit(async (effects) => {
   if (!(await configJson.read((c) => c.api.admin_password).const(effects))) {
-    await sdk.action.requestOwn(effects, resetPassword, 'critical', {
+    await sdk.action.createOwnTask(effects, resetPassword, 'critical', {
       reason: 'You must set an admin password to access your Datum UI',
     })
   }
