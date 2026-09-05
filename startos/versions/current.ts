c296e3d542a2cac2db4d52bffe2e3ce4a1507cf5
Léo Haf <leo@haf.ovh>
2026-09-05 11:25:34 +0200

    remove migration from sha256

--- startos/versions/current.ts
+++ startos/versions/current.ts
@@ -12,11 +12,5 @@ export const current = VersionInfo.of({
   migrations: {
     up: async ({ effects }) => {},
     down: async ({ effects }) => {},
-    other: {
-        ['*']: {
-            up: async ({ effects }) => {},
-            down: async ({ effects }) => {},
-        }
-    }
   },
 })
