6b251b315d361b4b12d3a3efbaab58e5a4975a9d
Léo Haf <leo@haf.ovh>
2026-09-06 09:01:37 +0200

    Bump to revision 21

--- startos/versions/current.ts
+++ startos/versions/current.ts
@@ -1,9 +1,9 @@
 import { VersionInfo } from '@start9labs/start-sdk'
 
 export const current = VersionInfo.of({
-  version: '#pow:0.4.1:20',
+  version: '#pow:0.4.1:21',
   releaseNotes: {
-    en_US: "Switch to convoy repo"
+    en_US: "Switch to convoy repo and fix migration bug"
   },
   migrations: {
     up: async ({ effects }) => {},
