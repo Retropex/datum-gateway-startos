158e83b25a37253c9657718cf89b4304320e7074
Léo Haf <leo@haf.ovh>
2026-09-06 15:34:26 +0200

    Bump to .22

--- startos/versions/current.ts
+++ startos/versions/current.ts
@@ -1,9 +1,9 @@
 import { VersionInfo } from '@start9labs/start-sdk'
 
 export const current = VersionInfo.of({
-  version: '#pow:0.4.1:21',
+  version: '#pow:0.4.1:22',
   releaseNotes: {
-    en_US: "Switch to convoy repo and fix migration bug"
+    en_US: "Fix compatibility issue with maveth pool"
   },
   migrations: {
     up: async ({ effects }) => {},
